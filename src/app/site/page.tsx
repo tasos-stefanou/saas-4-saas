5defa781ec2fe747e67bf8eb3ff55b91baf8adfb
Tasos Stefanou <tasos.stefanou.se@gmail.com>
2024-09-02 18:39:13 +0300

    create site basic page

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,9 +1,103 @@
-import { UserButton } from '@clerk/nextjs';
+import Image from 'next/image';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { Check } from 'lucide-react';
+import { pricingCards } from '@/lib/constants';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <UserButton />
-    </main>
+    <>
+      <section className='h-full w-full md:pt-44 relative flex items-center justify-center flex-col '>
+        <div className='absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10' />
+        <p className='text-center'>Run your agency, in one place</p>
+        <div className='bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative'>
+          <h1 className='text-9xl font-bold text-center md:text-[300px]'>
+            S44S
+          </h1>
+        </div>
+        <div className='flex justify-center items-center relative md:mt-[-70px]'>
+          <Image
+            src={'/assets/preview.png'}
+            alt='banner image'
+            height={1200}
+            width={1200}
+            className='rounded-tl-2xl rounded-tr-2xl border-2 border-muted'
+          />
+          <div className='bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10'></div>
+        </div>
+      </section>
+      <section className='flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]'>
+        <h2 className='text-4xl text-center'> Choose what fits you right</h2>
+        <p className='text-muted-foreground text-center'>
+          Our straightforward pricing plans are tailored to meet your needs. If
+          {" you're"} not <br />
+          ready to commit you can get started for free.
+        </p>
+        <div className='flex  justify-center gap-4 flex-wrap mt-6'>
+          {pricingCards.map((card) => (
+            //WIP: Wire up free product from stripe
+            <Card
+              key={card.title}
+              className={clsx('w-[300px] flex flex-col justify-between', {
+                'border-2 border-primary': card.title === 'Unlimited Saas',
+              })}
+            >
+              <CardHeader>
+                <CardTitle
+                  className={clsx('', {
+                    'text-muted-foreground': card.title !== 'Unlimited Saas',
+                  })}
+                >
+                  {card.title}
+                </CardTitle>
+                <CardDescription>
+                  {
+                    pricingCards.find((c) => c.title === card.title)
+                      ?.description
+                  }
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className='text-4xl font-bold'>{card.price}</span>
+                <span className='text-muted-foreground'>
+                  <span>/ {card.duration}</span>
+                </span>
+              </CardContent>
+              <CardFooter className='flex flex-col items-start gap-4'>
+                <div>
+                  {pricingCards
+                    .find((c) => c.title === card.title)
+                    ?.features.map((feature) => (
+                      <div key={feature} className='flex gap-2'>
+                        <Check />
+                        <p>{feature}</p>
+                      </div>
+                    ))}
+                </div>
+                <Link
+                  href={`/agency?plan=${card.priceId}`}
+                  className={clsx(
+                    'w-full text-center bg-primary p-2 rounded-md',
+                    {
+                      '!bg-muted-foreground': card.title !== 'Unlimited Saas',
+                    }
+                  )}
+                >
+                  Get Started
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </section>
+    </>
   );
 }
