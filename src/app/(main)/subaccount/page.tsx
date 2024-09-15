aba90090ffd70521f96a5007a7e7a9e2a34cb0ae
tasos-stefanou <tasos.stefanou.se@gmail.com>
2024-09-15 20:32:28 +0300

    add sub account page to redirect user to sub account page if permissions are correct

---
+++ src/app/(main)/subaccount/page.tsx
@@ -0,0 +1,41 @@
+import Unauthorized from '@/components/unauthorized';
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries';
+import { redirect } from 'next/navigation';
+
+type SubAccountMainPageProps = {
+  searchParams: { state: string; code: string };
+};
+
+const SubAccountMainPage = async ({
+  searchParams,
+}: SubAccountMainPageProps) => {
+  const agencyId = await verifyAndAcceptInvitation();
+
+  if (!agencyId) {
+    return <Unauthorized />;
+  }
+
+  const user = await getAuthUserDetails();
+  if (!user) return;
+
+  const getFirstSubaccountWithAccess = user.Permissions.find(
+    (permission) => permission.access === true
+  );
+
+  if (searchParams.state) {
+    const statePath = searchParams.state.split('___')[0];
+    const stateSubaccountId = searchParams.state.split('___')[1];
+    if (!stateSubaccountId) return <Unauthorized />;
+    return redirect(
+      `/subaccount/${stateSubaccountId}/${statePath}?code=${searchParams.code}`
+    );
+  }
+
+  if (getFirstSubaccountWithAccess) {
+    return redirect(`/subaccount/${getFirstSubaccountWithAccess.subAccountId}`);
+  }
+
+  return <Unauthorized />;
+};
+
+export default SubAccountMainPage;
