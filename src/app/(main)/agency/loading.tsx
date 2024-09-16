77f3ae7e06b26e483317c564501cc80d9844a82e
Tasos Stefanou <tasos.stefanou.se@gmail.com>
2024-09-16 18:28:36 +0300

    add loading page for agency

---
+++ src/app/(main)/agency/loading.tsx
@@ -0,0 +1,11 @@
+import Loading from '@/components/global/loading';
+
+const LoadingAgencyPage = () => {
+  return (
+    <div className='h-screen w-screen flex justify-center items-center'>
+      <Loading />
+    </div>
+  );
+};
+
+export default LoadingAgencyPage;
